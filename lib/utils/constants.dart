e4ac38b962f676a35e94092fac3d5f9975816592
amina-wq <kannami2004@gmail.com>
2026-07-10 22:49:20 +0800

    Use HTTPS CloudFront backend API

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   static const String appName = 'University Event Manager';
 
   static const String baseUrl =
-      'http://uem-ddac-backend-env.eba-arn85grs.ap-southeast-1.elasticbeanstalk.com/api';
+      'https://d30davekc2w0vq.cloudfront.net/api';
 
   static const String universityName = 'APU';
 }
